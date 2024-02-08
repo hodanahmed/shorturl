3baa6f72f7af7dd55dba6647ea20db185bd9567b
hodanahmed <hodanahmed00@gmail.com>
2024-02-08 14:51:11 +0000

    master: create full short urls

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-import express from "express";
-import mongoose from "mongoose";
-import ShortUrl from "./models/shortUrl.js";
-const app = express(); 
-const LOCAL_CONNECTION_URL = 'mongodb://localhost/urlShortener'
-
-
-mongoose.connect(process.env.PROD_CONNECTION_URL || LOCAL_CONNECTION_URL)
+import express from 'express';
+import mongoose from 'mongoose';
+import ShortUrl from './models/shortUrl.js';
+const LOCAL_CONNECTION_URL = 'mongodb://localhost/urlShortener';
+const HOST_NAME = process.env.HOST_NAME || 'http://localhost:3000';
+const app = express();
+mongoose.connect(process.env.PROD_CONNECTION_URL || LOCAL_CONNECTION_URL);
 
 //Middleware
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
-app.get("/", async (req, res) => {
-    const shortUrlsData = await ShortUrl.find()
-    res.render('index', {shortUrlsData})
-})
+app.get('/', async (req, res) => {
+  const shortUrlsData = await ShortUrl.find();
+  res.render('index', { shortUrlsData });
+});
 
-app.get("/:shortUrl", async(req, res) => {
-    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-    if (shortUrl === null) return res.sendStatus(404) // If no shortUrl exists, return 404 Not found
-    
-    shortUrl.clicks++ //Increments counter
-    shortUrl.save()
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({
+    short: `${HOST_NAME}/${req.params.shortUrl}`,
+  });
+  if (shortUrl === null) {
+    return res.sendStatus(404); // If no shortUrl exists, return 404 Not found
+  }
 
-    res.redirect(shortUrl.full)
-})
+  shortUrl.clicks++; //Increments counter
+  shortUrl.save();
 
-app.post("/shortUrls", async (req, res) => {
-    await ShortUrl.create({ full: req.body.fullUrl })
-    res.redirect("/")
-})
+  res.redirect(shortUrl.full);
+});
 
-//Server 
-app.listen(process.env.PORT || 3000, console.log("Server Started!"));
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect('/');
+});
+
+//Server
+app.listen(process.env.PORT || 3000, console.log('Server Started!'));
--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,20 +1,21 @@
 import mongoose from "mongoose";
 import { nanoid } from "nanoid";
+const HOST_NAME = process.env.HOST_NAME || 'http://localhost:3000';
 const shortUrlSchema = new mongoose.Schema({
-    full: {
-        type: String, 
-        required: true
-    },
-    short: {
-        type: String,
-        required: true,
-        default: nanoid(10)
-    },
-    clicks: {
-        type: Number,
-        required: true,
-        default: 0 
-    }
-})
+  full: {
+    type: String,
+    required: true,
+  },
+  short: {
+    type: String,
+    required: true,
+    default: `${HOST_NAME}/${nanoid(10)}`,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
 
 export default mongoose.model("ShortUrl", shortUrlSchema)
